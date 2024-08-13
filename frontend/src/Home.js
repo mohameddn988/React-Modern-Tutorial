430b47d2dd349b6494e826f86a2509507d89f29c
mohameddn988 <msideneche@gmail.com>
2024-08-13 18:04:09 +0100

    Using State (useState hook)

--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -1,7 +1,20 @@
+import { useState } from 'react'
+
+
 const Home = () => {
+    // let name = 'mario'
+    const [name, setName] = useState('mario')
+
+
+    const handleClick = () => {
+        setName('seif')
+        
+    }
     return ( 
         <div className="home">
             <h2>Home Page</h2>
+            <p>{ name }</p>
+            <button onClick={handleClick}>Click me</button>
         </div>
      );
 }
