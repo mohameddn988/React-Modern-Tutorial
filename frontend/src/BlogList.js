31fe835a38ae34a33e1ac9caab0d6ed3eac34b70
mohameddn988 <msideneche@gmail.com>
2024-08-13 21:10:14 +0100

    Props

---
+++ frontend/src/BlogList.js
@@ -0,0 +1,34 @@
+const BlogList = (props) => {
+    const blogs = props.blogsBs7Mbdla
+
+    console.log(blogs, props)
+
+
+    return ( 
+        <div className="blog-list">
+            {blogs.map((blog) => (
+                <div className="blog-preview" key={blog.id}>
+                    <h2>{blog.title}</h2>
+                    <p>Written by: {blog.author}</p>
+                </div>
+            ))}
+        </div>
+     );
+}
+ 
+export default BlogList;
+
+// Another way to do it
+// const BlogList = ({blogsBs7Mbdla}) => {
+
+//     return ( 
+//         <div className="blog-list">
+//             {blogs.map((blog) => (
+//                 <div className="blog-preview" key={blog.id}>
+//                     <h2>{blog.title}</h2>
+//                     <p>Written by: {blog.author}</p>
+//                 </div>
+//             ))}
+//         </div>
+//      );
+// }
