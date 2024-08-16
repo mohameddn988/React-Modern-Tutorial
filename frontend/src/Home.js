1d646da275500174eb93176945714d6e7e506b52
mohameddn988 <msideneche@gmail.com>
2024-08-16 19:02:37 +0100

    useEffect Dependencies

--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -9,21 +9,37 @@ const Home = () => {
         {title: 'Third Blog', body: 'This is the third blog post.', author: 'islem', id: 3}
     ])
 
+    
+    
+    
     const handleDelete = (id) => {
         const newBlogs = blogs.filter(blog => blog.id !== id)
         setBlogs(newBlogs) 
     }
 
+    // useEffect(() => {
+    //     console.log('use effect')
+    //     console.log(blogs)
+    // },) // run in every render
+    
+    // useEffect(() => {
+        //     console.log('use effect')
+        //     console.log(blogs)
+        // }, []) //only run in the first render 
+        
+        
+    const [name, setName] = useState('it me mariooooooooooooooooo')
+
     useEffect(() => {
         console.log('use effect')
-        console.log(blogs)
-    })
-
+        console.log(name)
+    }, [name]) //run every time the name changes
 
     return ( 
         <div className="home">
             <BlogList blogsBs7Mbdla={blogs} handleDelete={handleDelete} />
             {/* <BlogList blogsBs7Mbdla={blogs.filter((blog3) => blog3.author === 'islem')} handleDelete={handleDelete} /> */}
+            <button onClick={() => setName('oh noooooo')}>change name</button>
         </div>
     );
 }
